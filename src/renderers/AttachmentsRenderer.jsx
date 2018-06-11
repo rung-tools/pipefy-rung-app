770f217046c0fc903fbd2575f09831be77ae0e2d
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-06-11 09:54:58 -0300

    babel: use implicit function on renderers

--- src/renderers/AttachmentsRenderer.jsx
+++ src/renderers/AttachmentsRenderer.jsx
@@ -1,6 +1,6 @@
 import { h, render } from 'preact'
 import Attachments from '../components/Attachments'
 
-const AttachmentsRenderer = () => <Attachments pipefy={ PipefyApp } />
+const AttachmentsRenderer = ~<Attachments pipefy={ PipefyApp } />
 
 render(<AttachmentsRenderer />, document.getElementById('app'))
