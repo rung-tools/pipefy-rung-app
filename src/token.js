ca61dae80f74c27f21c1951a37905a6ca74eb03e
Marcelo Camargo <marcelocamargo@linuxmail.org>
2018-06-11 09:20:37 -0300

    Upload token.js

---
+++ src/token.js
@@ -0,0 +1,19 @@
+/* global PipefyApp */
+const { Promise } = PipefyApp
+
+/* global window */
+export const getToken = pipefy => new Promise((resolve, reject) => {
+    const key = `rung-token-${pipefy.pipeId}`
+    const cachedToken = window.sessionStorage.getItem(key)
+
+    if (cachedToken && cachedToken !== 'null') {
+        return resolve(cachedToken)
+    }
+
+    return pipefy.get('pipe', 'private', 'token')
+        .then(token => {
+            window.sessionStorage.setItem(key, token)
+            resolve(token)
+        })
+        .catch(reject)
+})
